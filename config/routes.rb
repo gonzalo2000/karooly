e8d26d3c014732b1bb4fdf47ed06f8c884b8c8a5
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-05-25 21:23:54 -0400

    Add lessons index & navbar link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :lessons, only: [:index, :show]
   namespace :teacher do
     resources :lessons
   end
