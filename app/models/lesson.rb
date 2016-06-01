16cd5907b65fa5f4ffecd53dfff6258633a90f2a
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-05-31 21:21:12 -0400

    Add validations to lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,8 +1,8 @@
 class Lesson < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :subject, presence: true
-  validates :difficulty, presence: true
+  validates :title, presence: true, length: { maximum: 55 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :subject, presence: true, length: { maximum: 55 }
+  validates :difficulty, presence: true, numericality: { less_than_or_equal_to: 5 }
 end
