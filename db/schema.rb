76c286d42861bf6c8d766a91e62d518e9ecf2363
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-06-11 16:24:59 -0400

    Add username functionality to devise, and display username in student lesson index and show, as well as navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608231130) do
+ActiveRecord::Schema.define(version: 20160611194609) do
 
   create_table "enrollments", force: :cascade do |t|
     t.integer  "user_id"
@@ -48,10 +48,12 @@ ActiveRecord::Schema.define(version: 20160608231130) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "words", force: :cascade do |t|
     t.string   "term"
