8c3d6efb8986792a68903d593e18127cdb3d6baf
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-06-14 23:12:29 -0400

    Add previous/next links in student word show views

--- app/models/word.rb
+++ app/models/word.rb
@@ -8,4 +8,12 @@ class Word < ActiveRecord::Base
   validates :reference, presence: true, length: { maximum: 55 }
   validates :image, presence: true
   validates :sound, presence: true
+
+  def previous
+    Word.where(["id < ?", id]).last
+  end
+
+  def next
+    Word.where(["id > ?", id]).first
+  end
 end
