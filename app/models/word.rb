56b0a4e06d70c2b00eaec342f50029105924e552
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-06-20 19:59:40 -0400

    Strip whitespace before saving Word

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,7 @@
 class Word < ActiveRecord::Base
   belongs_to :lesson
   has_many :word_expositions, dependent: :destroy
+  before_save :strip_whitespace
 
   mount_uploader :image, ImageUploader
   mount_uploader :sound, SoundUploader
@@ -17,4 +18,10 @@ class Word < ActiveRecord::Base
   def next
     Word.where(["id > ? AND lesson_id = ?", id, lesson_id]).first
   end
+
+  private
+    def strip_whitespace
+      self.term = self.term.split.join(' ')
+      self.reference = self.reference.split.join(' ')
+    end
 end
