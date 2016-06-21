c58cb93cfb36e179d723f7308b698bc67e8dbcce
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-06-20 22:21:40 -0400

    Add split & join so word_from_student_matches_word checks for spelling irregardless of whitespace

--- app/models/word_exposition.rb
+++ app/models/word_exposition.rb
@@ -11,7 +11,7 @@ class WordExposition < ActiveRecord::Base
   validate :word_from_student_matches_word, on: :update
 
   def word_from_student_matches_word
-    return true if word.term == term_given_by_student
+    return true if word.term == term_given_by_student.split.join(' ')
     errors.add(:term_given_by_student, "Terms don't match")
   end
 
@@ -19,4 +19,5 @@ class WordExposition < ActiveRecord::Base
   def next_exposition
     WordExposition.where(["id > ? AND enrollment_id = ?", id, enrollment_id]).first
   end
+
 end
