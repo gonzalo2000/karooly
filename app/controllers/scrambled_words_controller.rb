207e80a13f1bdd5667b437b7484fe7039853fea5
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-06-24 18:44:24 -0400

    Add bug comment to scrambled words controller

--- app/controllers/scrambled_words_controller.rb
+++ app/controllers/scrambled_words_controller.rb
@@ -10,7 +10,7 @@ class ScrambledWordsController < ApplicationController
     @word = current_enrollment.scrambled_words.find_by!(word_id: params[:id])
   end
 
-  #need to implement show next scrambled word upon succesful submission
+  #bug, not showing all scrambled words before showing complete notice
   def update
     current_scrambled_word
     @current_scrambled_word.unscrambled_attempt = params[:scrambled_word][:unscrambled_attempt]
