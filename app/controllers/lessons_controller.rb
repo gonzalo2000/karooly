494d75d1ede5b2acf6027a9a6c4a95d5851ace74
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-06-26 23:47:48 -0400

    Add button to student lesson show page if exposition_completed, and associated lesson controller helper method

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,4 +6,13 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
   end
+
+  helper_method :completed_expo
+  def completed_expo
+    lesson = Lesson.find(params[:id])
+    if current_enrollment = lesson.enrollment_for(current_user)
+      all_expositions = current_enrollment.word_expositions
+      all_expositions.all? { |word_expo| word_expo.completed == true }
+    end
+  end
 end
