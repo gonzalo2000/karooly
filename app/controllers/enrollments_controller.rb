76facc0eb6c68233bd97fcd8f866f36dd65abf79
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-08-01 23:32:23 -0400

    add before_action to enrollments, to prevent enrollment into empty lessons

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,5 +1,6 @@
 class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :lesson_empty?, only: [:create]
 
   def create
     current_user.enrollments.create(lesson: current_lesson)
@@ -10,5 +11,12 @@ class EnrollmentsController < ApplicationController
 
   def current_lesson
     @current_lesson ||= Lesson.find(params[:lesson_id])
+  end
+
+  def lesson_empty?
+    if current_lesson.words.empty?
+      flash[:alert] = "You may not enroll in lessons without vocabulary."
+      redirect_to lessons_path
+    end
   end
 end
