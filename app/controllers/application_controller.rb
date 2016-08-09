7cd576bfbb8c63601b55e97ab1bd2f8f83683987
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-08-08 21:33:08 -0400

    Redirect to lessons after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,9 @@ class ApplicationController < ActionController::Base
    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-  end 
+  end
+
+  def after_sign_in_path_for(resource)
+    lessons_path(current_user) 
+  end
 end
