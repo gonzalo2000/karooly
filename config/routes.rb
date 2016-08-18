eeca597307c3fbdb83a58ace1326655c2f8eda65
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-08-17 22:20:06 -0400

    Add empty About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  get 'about', to: 'static_pages#about'
+  
   resource :dashboard, only: [:show]
   resources :lessons, only: [:index, :show] do 
     resources :enrollments, only: :create
