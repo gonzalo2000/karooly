0b8024abf7d4905ff83f509011e2ead367f2a594
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-08-23 19:54:22 -0400

    Set lesson and enrollments order for dashboard in its controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,7 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
   
   def show
+    @user_lessons = current_user.lessons.order('created_at ASC')
+    @user_enrolled_lessons = current_user.enrolled_lessons.order('created_at ASC')
   end
 end
