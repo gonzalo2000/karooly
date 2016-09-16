d2b5dc07ca81d4d94850b0d6e0e10fd86ccd95ab
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-09-15 23:05:06 -0400

    Refactor and comment out lessons controller code, simplify lesson show view

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -25,39 +25,35 @@ class LessonsController < ApplicationController
     end
   end
 
-  helper_method :completed_expo
-  def completed_expo
-    lesson = Lesson.find(params[:id])
+  helper_method :expositions_complete?
+  def expositions_complete?
+    lesson ||= Lesson.find(params[:id])
     if current_enrollment = lesson.enrollment_for(current_user)
-      all_expositions = current_enrollment.word_expositions
-      all_expositions.all? { |word_expo| word_expo.completed == true }
+      current_enrollment.expositions_completed
     end
   end
 
-  helper_method :completed_unscramble
-  def completed_unscramble
-    lesson = Lesson.find(params[:id])
+  helper_method :unscrambled_complete?
+  def unscrambled_complete?
+    lesson ||= Lesson.find(params[:id])
     if current_enrollment = lesson.enrollment_for(current_user)
-      all_unscramble = current_enrollment.scrambled_words
-      all_unscramble.all? { |scrambled| scrambled.completed == true }
+      current_enrollment.unscrambled_completed
     end
   end
 
-  helper_method :completed_dictations
-  def completed_dictations
-    lesson = Lesson.find(params[:id])
+  helper_method :dictation_complete?
+  def dictation_complete?
+    lesson ||= Lesson.find(params[:id])
     if current_enrollment = lesson.enrollment_for(current_user)
-      all_dictations = current_enrollment.word_dictations
-      all_dictations.all? { |dictation| dictation.completed == true }
+      current_enrollment.dictation_completed
     end
   end
 
-  helper_method :completed_spellings
-  def completed_spellings
-    lesson = Lesson.find(params[:id])
+  helper_method :image_spellings_complete?
+  def image_spellings_complete?
+    lesson ||= Lesson.find(params[:id])
     if current_enrollment = lesson.enrollment_for(current_user)
-      all_spellings = current_enrollment.image_spellings
-      all_spellings.all? { |spelling| spelling.completed == true }
+      current_enrollment.image_spelling_completed
     end
   end
 
@@ -67,48 +63,88 @@ class LessonsController < ApplicationController
     enrollments = lesson.enrollments
     enrolled_users = enrollments.map { |enrollment| enrollment.user }
   end
+  
+  #this helper method will be phased out
+  # helper_method :completed_expo
+  # def completed_expo
+  #   lesson = Lesson.find(params[:id])
+  #   if current_enrollment = lesson.enrollment_for(current_user)
+  #     all_expositions = current_enrollment.word_expositions
+  #     all_expositions.all? { |word_expo| word_expo.completed == true }
+  #   end
+  # end
 
-  #for all users
-  helper_method :completed_scram
-  def completed_scram
-    lesson = Lesson.find(params[:id])
-    completed_scram = enrolled_users.map do |user|
-      current_enrollment = lesson.enrollment_for(user)
-      all_unscramble = current_enrollment.scrambled_words
-      user if all_unscramble.all? { |scrambled| scrambled.completed == true }
-    end
-  end
+  # #this helper method will be phased out
+  # helper_method :completed_unscramble
+  # def completed_unscramble
+  #   lesson = Lesson.find(params[:id])
+  #   if current_enrollment = lesson.enrollment_for(current_user)
+  #     all_unscramble = current_enrollment.scrambled_words
+  #     all_unscramble.all? { |scrambled| scrambled.completed == true }
+  #   end
+  # end
 
-  #for all users
-  helper_method :completed_expos
-  def completed_expos
-    lesson = Lesson.find(params[:id])
-    completed_expos = enrolled_users.map do |user|
-      current_enrollment = lesson.enrollment_for(user)
-      all_expos = current_enrollment.word_expositions
-      user if all_expos.all? { |expos| expos.completed == true }
-    end
-  end
+  # #this helper method will be phased out
+  # helper_method :completed_dictations
+  # def completed_dictations
+  #   lesson = Lesson.find(params[:id])
+  #   if current_enrollment = lesson.enrollment_for(current_user)
+  #     all_dictations = current_enrollment.word_dictations
+  #     all_dictations.all? { |dictation| dictation.completed == true }
+  #   end
+  # end
 
-  #for all users
-  helper_method :completed_dicts
-  def completed_dicts
-    lesson = Lesson.find(params[:id])
-    completed_dic = enrolled_users.map do |user|
-      current_enrollment = lesson.enrollment_for(user)
-      all_dicts = current_enrollment.word_dictations
-      user if all_dicts.all? { |dict| dict.completed == true }
-    end
-  end
+  # #this helper method will be phased out
+  # helper_method :completed_spellings
+  # def completed_spellings
+  #   lesson = Lesson.find(params[:id])
+  #   if current_enrollment = lesson.enrollment_for(current_user)
+  #     all_spellings = current_enrollment.image_spellings
+  #     all_spellings.all? { |spelling| spelling.completed == true }
+  #   end
+  # end
 
-  #for all users
-  helper_method :completed_spel
-  def completed_spel
-    lesson = Lesson.find(params[:id])
-    completed_sp = enrolled_users.map do |user|
-      current_enrollment = lesson.enrollment_for(user)
-      all_sp = current_enrollment.image_spellings
-      user if all_sp.all? { |spell| spell.completed == true }
-    end
-  end
+  # #for all users
+  # helper_method :completed_scram
+  # def completed_scram
+  #   lesson = Lesson.find(params[:id])
+  #   completed_scram = enrolled_users.map do |user|
+  #     current_enrollment = lesson.enrollment_for(user)
+  #     all_unscramble = current_enrollment.scrambled_words
+  #     user if all_unscramble.all? { |scrambled| scrambled.completed == true }
+  #   end
+  # end
+
+  # #for all users
+  # helper_method :completed_expos
+  # def completed_expos
+  #   lesson = Lesson.find(params[:id])
+  #   completed_expos = enrolled_users.map do |user|
+  #     current_enrollment = lesson.enrollment_for(user)
+  #     all_expos = current_enrollment.word_expositions
+  #     user if all_expos.all? { |expos| expos.completed == true }
+  #   end
+  # end
+
+  # #for all users
+  # helper_method :completed_dicts
+  # def completed_dicts
+  #   lesson = Lesson.find(params[:id])
+  #   completed_dic = enrolled_users.map do |user|
+  #     current_enrollment = lesson.enrollment_for(user)
+  #     all_dicts = current_enrollment.word_dictations
+  #     user if all_dicts.all? { |dict| dict.completed == true }
+  #   end
+  # end
+
+  # #for all users
+  # helper_method :completed_spel
+  # def completed_spel
+  #   lesson = Lesson.find(params[:id])
+  #   completed_sp = enrolled_users.map do |user|
+  #     current_enrollment = lesson.enrollment_for(user)
+  #     all_sp = current_enrollment.image_spellings
+  #     user if all_sp.all? { |spell| spell.completed == true }
+  #   end
+  # end
 end
