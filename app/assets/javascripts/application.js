30d5159ada27103b4435c573fcd77846e61491c3
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-09-18 14:43:40 -0400

    Add toggle for German and Spanish special characters in activity views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,177 @@ jQuery(document).ready(function() {
 
 });
 
+
+jQuery(document).ready(function() {
+  $(".accents-btn").click(function(){
+    $(".show-accents").toggle();
+  });
+
+  $("#a-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "á";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#e-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "é";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#i-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "í";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#o-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "ó";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#u-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "ú";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#n-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "ñ";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#u-accent-dier").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "ü";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#A-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Á";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#E-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "É";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#I-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Í";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#O-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Ó";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#U-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Ú";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#N-accent").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Ñ";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#U-accent-dier").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Ü";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#a-german").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "ä";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#o-german").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "ö";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#u-german").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "ü";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#eszett-german").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "ß";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#A-german").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Ä";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#O-german").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Ö";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+
+  $("#U-german").on('click', function() {
+    var $txt = jQuery("#txt");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "Ü";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+  });
+});
